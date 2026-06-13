73c1c50221c85af98a24a9f78335883565014933
Barbarayoussef <barayoussef12@yahoo.com>
2026-06-13 16:37:50 +0300

    Add post model schema with title, content, and authorId fields

---
+++ src/database/models/post.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const postModel = mongoose.model("post", postSchema);
+
+export default postModel;
